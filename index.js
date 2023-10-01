eb1ed3b50093e5c62b1ef09cdcce82e06832424e
advmaster <118003523+advmaster@users.noreply.github.com>
2023-09-30 23:53:43 -0300

    a

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(router);
 
 
 // Use PORT provided in environment or default to 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Listen on `port` and 0.0.0.0
 app.listen(port, "0.0.0.0");
