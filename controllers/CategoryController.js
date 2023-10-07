362b2f344ef5c08ffbd176edeac8d2f6ef52e7b2
advmaster <118003523+advmaster@users.noreply.github.com>
2023-10-07 20:40:48 -0300

    a

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -4,51 +4,52 @@ const db = new PrismaClient()
 
 class CategoryController {
   
-  constructor() {}
+  constructor () {
 
-  async add(req, res) {
-    try {
-      const newCategory = await db.category.create({
-        data: {
-          name: req.body.name,
-          userId: parseInt(req.body.userId)
-        }
-      })
-      return res.status(201).json({ message: 'Category added successfully', data: newCategory });
-    } catch (error) {
-      return res.status(500).json({ message: 'Error adding category', error: error.message });
-    }
   }
 
+
+  async add(req, res) {
+    const newCategory = await db.category.create({
+      data: {
+        name: req.body.name,
+        userId: parseInt(req.body.userId)
+      }
+    })
+    return res.json(newCategory)
+  }
+
+
   async getCategoriesByUserId(req, res) {
-    try {
-      const user = req.query;
-      const categories = await db.category.findMany({
-        where: {
-          userId: {
-            equals: parseInt(user.id)
-          }
+
+    const user = req.query
+
+    const categories = await db.category.findMany({
+      where: {
+        userId: {
+          equals: parseInt(user.id)
         }
-      })
-      return res.json(categories)
-    } catch (error) {
-      return res.status(500).json({ message: 'Error fetching categories', error: error.message });
-    }
+      }
+    })
+
+    return res.json(categories)
+
   }
 
   async delete(req, res) {
-    try {
-      const category = req.params;
-      await db.category.delete({
-        where: {
-          id: parseInt(category.id)
+
+    const category = req.params;
+
+     await db.category.delete({
+          where: {
+            id: parseInt(category.id)
         }
       });
-      return res.status(200).json({ message: 'Category deleted successfully' });
-    } catch (error) {
-      return res.status(500).json({ message: 'Error deleting category', error: error.message });
-    }
+
+      return res.json({ message: 'Category deleted successfully' })
+
   }
+
 }
 
 export default CategoryController;
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,30 +4,27 @@ const db = new PrismaClient()
 
 class UserController {
   
-  constructor() {}
+  constructor () {
+
+  }
 
   async getAll(req, res) {
-    try {
-      const users = await db.user.findMany();
-      return res.status(200).json({ message: 'Users fetched successfully', data: users });
-    } catch (error) {
-      return res.status(500).json({ message: 'Error fetching users', error: error.message });
-    }
+    const users = await db.user.findMany()
+    return res.status(200).json(users)
   }
 
   async add(req, res) {
-    try {
-      const user = req.body;
-      const newUser = await db.user.create({
-        data: {
-          email: user.email,
-          name: user.name
-        }
-      });
-      return res.status(201).json({ message: 'User added successfully', data: newUser });
-    } catch (error) {
-      return res.status(500).json({ message: 'Error adding user', error: error.message });
-    }
+
+
+    const user = req.body;
+
+    const newUser = await db.user.create({
+      data: {
+        email: user.email,
+        name: user.name
+      }
+    })
+    return res.json(newUser)
   }
 
 }
