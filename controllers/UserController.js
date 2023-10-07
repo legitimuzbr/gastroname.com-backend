8937831a7abdc9fcd414f45783e012807e352b37
advmaster <118003523+advmaster@users.noreply.github.com>
2023-10-07 20:28:20 -0300

    a

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,27 +4,30 @@ const db = new PrismaClient()
 
 class UserController {
   
-  constructor () {
-
-  }
+  constructor() {}
 
   async getAll(req, res) {
-    const users = await db.user.findMany()
-    return res.status(200).json(users)
+    try {
+      const users = await db.user.findMany();
+      return res.status(200).json({ message: 'Users fetched successfully', data: users });
+    } catch (error) {
+      return res.status(500).json({ message: 'Error fetching users', error: error.message });
+    }
   }
 
   async add(req, res) {
-
-
-    const user = req.body;
-
-    const newUser = await db.user.create({
-      data: {
-        email: user.email,
-        name: user.name
-      }
-    })
-    return res.json(newUser)
+    try {
+      const user = req.body;
+      const newUser = await db.user.create({
+        data: {
+          email: user.email,
+          name: user.name
+        }
+      });
+      return res.status(201).json({ message: 'User added successfully', data: newUser });
+    } catch (error) {
+      return res.status(500).json({ message: 'Error adding user', error: error.message });
+    }
   }
 
 }
